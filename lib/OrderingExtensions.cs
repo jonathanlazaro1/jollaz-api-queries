cf73100cede5fc4d549e6b6b0bc5d46158adf761
Jonathan Lazzaro <jonathan.lazaro1@gmail.com>
2019-01-05 17:04:55 -0400

    Method summary in SelectByDataRequest and OrderByDataRequest.

--- lib/OrderingExtensions.cs
+++ lib/OrderingExtensions.cs
@@ -8,15 +8,29 @@ namespace JollazApiQueries.Library.Extensions
 {
     public static class OrderingExtensions
     {
-        public static IQueryable<T> OrderByDataRequest<T>(this IQueryable<T> query, DataRequest request)
+        ///<summary>
+        /// Extension method that allows query ordering based on a data request.
+        /// <param name="query">The query where the ordering will be applied.</param>
+        /// <param name="dataRequest">The data request, which contains the property names that will be used to sort the query.</param>
+        /// <typeparam name="T">The query data type.</typeparam>
+        /// <returns>Returns the query with the ordering applied and ready to fetch.</returns>
+        ///</summary>
+        public static IQueryable<T> OrderByDataRequest<T>(this IQueryable<T> query, DataRequest dataRequest)
         {
-            foreach (var ordering in request.Ordering.Reverse())
+            foreach (var ordering in dataRequest.Ordering.Reverse())
             {
                 query = query.OrderByOrderingItem<T>(ordering);
             }
             return query;
         }
 
+        ///<summary>
+        /// Extension method that allows query ordering based on an ordering item.
+        /// <param name="query">The query where the ordering will be applied.</param>
+        /// <param name="ordering">The ordering item that will be parsed and applied to the query.</param>
+        /// <typeparam name="T">The query data type.</typeparam>
+        /// <returns>Returns the query with the ordering applied and ready to fetch.</returns>
+        ///</summary>
         private static IQueryable<T> OrderByOrderingItem<T>(this IQueryable<T> query, OrderingItem ordering)
         {
             try
--- lib/SelectExtensions.cs
+++ lib/SelectExtensions.cs
@@ -8,9 +8,16 @@ namespace JollazApiQueries.Library.Extensions
 {
     public static class SelectExtensions
     {
-        public static IQueryable SelectByDataRequest<T>(this IQueryable<T> query, DataRequest request)
+        ///<summary>
+        /// Extension method that allows selecting custom properties in a query, based on a Data Request.
+        /// <param name="query">The query where the selecting will be applied.</param>
+        /// <param name="dataRequest">The data request, which contains the property names that will be used to select query properties.</param>
+        /// <typeparam name="T">The query data type.</typeparam>
+        /// <returns>Returns an object of type IQueryable, which is a collection of anonymous objects with the properties selected before.</returns>
+        ///</summary>
+        public static IQueryable SelectByDataRequest<T>(this IQueryable<T> query, DataRequest dataRequest)
         {
-            string ret = string.Join(", ", request.Select);
+            string ret = string.Join(", ", dataRequest.Select);
             try
             {
                 return query.Select($"new ({ret})");
